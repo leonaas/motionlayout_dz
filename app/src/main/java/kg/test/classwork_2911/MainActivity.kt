c3704858b889574e026d13bc1dd744bdf5a0f1dc
leonaasanova <asanova_l@auca.kg>
2021-12-04 19:08:19 +0600

    commit

---
+++ app/src/main/java/kg/test/classwork_2911/MainActivity.kt
@@ -0,0 +1,11 @@
+package kg.test.classwork_2911
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/kg/test/classwork_2911/homework_0212.kt
@@ -0,0 +1,11 @@
+package kg.test.classwork_2911
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class homework_0212 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_homework0212)
+    }
+}
